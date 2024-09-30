de7d1e6e3a398b7980558668dc7e9388163c4e6f
Ismail Ahmed <aismail@kictl.com>
2024-10-01 02:03:54 +0500

    first commit

---
+++ app/global.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_OMDB_API_KEY: string;
+    }
+  }
+  
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['m.media-amazon.com'], // Add external image domain here
+    },
+  };
+  
+  export default nextConfig;
+  
